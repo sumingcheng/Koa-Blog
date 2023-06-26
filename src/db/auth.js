8f05f5ae37ec5bb0d0b303b95051dd5caf447ab6
范晓豪 <fanxiaohaohaha@qq.com>
2023-06-26 14:24:50 +0800

    fix：简化代码

--- src/db/auth.js
+++ src/db/auth.js
@@ -3,7 +3,7 @@ const MySQL = require('./index');
 // 使用查询函数
 async function main () {
   try {
-    const [rows, fields] = await MySQL.execute('SELECT * FROM `user`')
+    const [rows] = await MySQL.execute('SELECT * FROM `user`')
     console.log(rows);
   } catch (error) {
     console.error(error);
--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,22 @@
-const MySQL = require('./connect');
 const {logger} = require('../middleware/logger');
+const mysql = require('mysql2/promise');
+
+// 创建一个连接池
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  database: 'blog',
+  password: '123456',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  idleTimeout: 60000,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
+});
 
 // 测试连接
-MySQL.getConnection()
+pool.getConnection()
     .then(conn => {
       conn.release();
       logger.info('Successfully connected to the database.');
@@ -11,4 +25,4 @@ MySQL.getConnection()
       logger.error('Failed to connect to the database: ', err);
     });
 
-module.exports = MySQL
+module.exports = pool
